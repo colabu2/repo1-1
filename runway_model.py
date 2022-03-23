fc7e6d498b18ddfd532e81a11dd2819f000c0b39
colabu11 <101316095+colabu11@users.noreply.github.com>
2022-03-23 14:46:37 +0800

    Update runway_model.py

--- runway_model.py
+++ runway_model.py
@@ -1,8 +1,8 @@
 import subprocess
 import requests
 
-url = 'wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip'
+url = 'wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz'
 r = requests.get(url, allow_redirects=True)
 
-open('ngrok-stable-linux-amd64.zip', 'wb').write(r.content)
+open('ngrok-stable-linux-amd64.tgz', 'wb').write(r.content)
 subprocess.call("ls", shell=True)
