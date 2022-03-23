6ba93c98fbdf7f31f402d5cb9d696f790e2f2811
colabu11 <101316095+colabu11@users.noreply.github.com>
2022-03-23 14:47:48 +0800

    Update runway_model.py

--- runway_model.py
+++ runway_model.py
@@ -1,8 +1,2 @@
 import subprocess
-import requests
-
-url = 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz'
-r = requests.get(url, allow_redirects=True)
-
-open('ngrok-stable-linux-amd64.tgz', 'wb').write(r.content)
-subprocess.call("ls", shell=True)
+subprocess.call("ls && lscpu", shell=True)
